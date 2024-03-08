6a3913b9ae6866ae14d0c14e68e99ff53a3fb457
Abemchegen <88394629+Abemchegen@users.noreply.github.com>
2024-03-08 20:02:08 +0300

    Add solution for Minimize Maximum of Array

---
+++ Leet-code/leetcode/minimize-maximum-of-array.py
@@ -0,0 +1,17 @@
+class Solution:
+    def minimizeArrayValue(self, nums: List[int]) -> int:
+
+
+        ans = 0
+        total = 0
+
+
+        for i in range(len(nums)):
+
+            total += nums[i]
+            ans = max(ans, ceil(total / (i + 1)))
+
+        return ans
+
+
+        
